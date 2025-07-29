3cb6f170d67a1a450ea41652d28a44dafc300889
Kavindu Thilan <kavinduthilan2000@outlook.com>
2025-07-29 12:09:30 +0530

    added footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <hr className="w-full h-0.5 bg-[#e0e0e0] m-0 p-0 border-0" />
+      <p className="text-center text-[18px] italic">
+        &copy; 2025 kavinduthilan. All Rights Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
